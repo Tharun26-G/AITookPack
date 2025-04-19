ebb660b1fce19e540d653240f4ef1afecdf01b86
Tharun26-G <tharun26.g@gmail.com>
2025-04-19 18:41:35 +0530

    unique validation

--- Backend/models/Tool.js
+++ Backend/models/Tool.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const ToolSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  url: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
+  url: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   categories: [{ type: String }],
   price: { type: String, required: true },
